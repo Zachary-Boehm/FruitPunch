ef53084fb2b3bd405dc593172c83726cf36cac5e
tomcat6000 <boehmz8613@my.uwstout.edu>
2021-03-16 20:50:03 -0500

    Added all current animations

--- Assets/Scripts/AI & Object Info/AI/EnemyMovement.cs
+++ Assets/Scripts/AI & Object Info/AI/EnemyMovement.cs
@@ -7,10 +7,11 @@ public class EnemyMovement : MonoBehaviour
     public Transform Player;
     int MoveSpeed = 30;
     int MaxDist = 10;
-    int MinDist = 5;
+    int MinDist = 10;
 
 
     Vector3 movement = Vector3.zero;
+    [SerializeField] private Vector2 Direction;
     void Start()
     {
 
@@ -18,14 +19,21 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
+        Direction = (Player.position - transform.position).normalized;
         if (Vector3.Distance(transform.position, Player.position) >= MinDist)
         {
-          movement = (Player.position - transform.position).normalized * MoveSpeed * Time.deltaTime;
+          movement =  Direction * MoveSpeed * Time.deltaTime;
           movement.z = 0;
         }
         else
         {
+          Direction = Vector2.zero;
           movement = Vector3.zero;
+        }
+        
+        GetComponent<StaticVariables>().Direction = Direction;
+        if(Direction.x != 0){
+          GetComponent<StaticVariables>().AttackDirection = Direction;
         }
     }
     private void FixedUpdate() {
--- Assets/Scripts/Combat/DamageType.cs
+++ Assets/Scripts/Combat/DamageType.cs
@@ -10,6 +10,6 @@ public enum DamageAffect{
   Slow = 1, //Will slow target
   Stun = 2, //Will stun target
   Bleed = 3, //Will apply bleed affect to the target
-  Other = 0//Default state
+  Default = 0//Default state
 }
 
