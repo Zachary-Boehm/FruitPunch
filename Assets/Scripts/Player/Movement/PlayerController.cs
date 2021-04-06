b73683ff5cbb3649348adecc5ac448cba24e2351
tomcat6000 <boehmz8613@my.uwstout.edu>
2021-04-06 16:08:26 -0500

    Fixed some minor bugs

--- Assets/Scripts/Player/Movement/PlayerController.cs
+++ Assets/Scripts/Player/Movement/PlayerController.cs
@@ -7,88 +7,113 @@ using UnityEngine;
 [RequireComponent(typeof(ActorVariables))]
 public class PlayerController : MonoBehaviour
 {
-  [Header("Player Attributes")]
-  [SerializeField] private Vector2 Velocity; //Velocity based on input
- 
-  [Header("Player Constants")]
-  [SerializeField] private ActorVariables Variables;
-   [SerializeField] private Transform Transform; //Reference to the players transform
-  [SerializeField]private Rigidbody2D PlayerRigidBody;//Reference to the Rigid body of the player
-  [SerializeField] private Vector2 direction;//direction the player is moving in
-  
-  //Attack variables
-  [Header("Attack Variables")]
-  private bool AttackPressed = false;
-  [SerializeField] private float chargeMultiplier;
-  [SerializeField] private float chargeTime;
-  private float chargeAmount = 0.0f;//percent of how much the player charged the attack
-  //bool canMove = true;
+    [Header("Player Attributes")]
+    [SerializeField] private Vector2 Velocity; //Velocity based on input
 
-  private void Awake() {
-    Velocity = Vector2.zero; //Initialize the Velocity to (0,0)
-    Transform = GetComponent<Transform>();//Initialize to transform of player
-    PlayerRigidBody = GetComponent<Rigidbody2D>(); //Initialize to the rigidbody of the player
-  }
+    [Header("Player Constants")]
+    [SerializeField] private ActorVariables Variables;
+    [SerializeField] private Transform Transform; //Reference to the players transform
+    [SerializeField] private Rigidbody2D PlayerRigidBody;//Reference to the Rigid body of the player
+    [SerializeField] private Vector2 direction;//direction the player is moving in
 
-  private void Update() {
-    //inputs
-    //Set the X of Velocity to the horizontal input * speed
-    Velocity.x = Input.GetAxisRaw("Horizontal") * Variables.moveSpeed;
-    //Set the Y of Velocity to the vertical input * speed
-    Velocity.y = Input.GetAxisRaw("Vertical") * Variables.moveSpeed;
+    //Attack variables
+    [Header("Attack Variables")]
+    private bool AttackPressed = false;
+    [SerializeField] private float chargeMultiplier;
+    [SerializeField] private float chargeTime;
+    private bool canAttack = true;
+    private float chargeAmount = 0.0f;//percent of how much the player charged the attack
+                                      //bool canMove = true;
 
-    if(GameManager.GAMEMANAGER.getCanMove() && !Variables.isAttacking){
-      //!Optimize Here
-      direction = Velocity.normalized;
-      Variables.Direction = direction;
-      if(direction.x != 0){
-        Variables.AttackDirection = direction;
-      }
-    }else{
-      direction = Vector2.zero;
-    }
-    
-
-    //Check for attack button to be pressed
-    if(Input.GetAxisRaw("Fire1") > 0.01 && !AttackPressed){
-      AttackPressed = true;
-      //Button pressed
-      StartCoroutine(chargeAttack());//Start a coroutine that will charge a attack over time
-    }else if(Input.GetAxisRaw("Fire1") == 0 && AttackPressed == true){
-      AttackPressed = false;
-      //Button Released
-      releaseAttack();
-    }
-  }
-
-  private void FixedUpdate() {
-    //Apply motion
-    //Set the velocity of the player's rigid body to the Velocity(input) * the change in time between frames
-    if(!Variables.isAttacking && GameManager.GAMEMANAGER.getCanMove()) {
-      PlayerRigidBody.velocity = Velocity * Time.deltaTime;
-    }else{
-      PlayerRigidBody.velocity = Vector2.zero;
-    }
-  }
-
-  IEnumerator chargeAttack()
-  {
-    Variables.isAttacking = true;
-    GetComponent<AnimationController>().ChangeAnim(SceneConstants.PunchChargeAnim);
-    while(chargeAmount < 100)
+    private void Awake()
     {
-      yield return new WaitForSeconds(chargeTime/100);
-      chargeAmount += 1f;
+        Velocity = Vector2.zero; //Initialize the Velocity to (0,0)
+        Transform = GetComponent<Transform>();//Initialize to transform of player
+        PlayerRigidBody = GetComponent<Rigidbody2D>(); //Initialize to the rigidbody of the player
     }
-  }
-  private void releaseAttack(){
-    StopAllCoroutines();//Stop charging the attack
-    float mult = (chargeAmount / 100) * chargeMultiplier; //Damage mutliplier for charge attack
-    if(mult < 1){//Damage will at least be 1
-      mult = 1;
+
+    private void Update()
+    {
+        //inputs
+        //Set the X of Velocity to the horizontal input * speed
+        Velocity.x = Input.GetAxisRaw("Horizontal") * Variables.moveSpeed;
+        //Set the Y of Velocity to the vertical input * speed
+        Velocity.y = Input.GetAxisRaw("Vertical") * Variables.moveSpeed;
+
+        if (GameManager.GAMEMANAGER.getCanMove() && !Variables.isAttacking)
+        {
+            //!Optimize Here
+            direction = Velocity.normalized;
+            Variables.Direction = direction;
+            if (direction.x != 0)
+            {
+                Variables.AttackDirection = direction;
+            }
+        }
+        else
+        {
+            direction = Vector2.zero;
+        }
+
+
+        //Check for attack button to be pressed
+        if (Input.GetAxisRaw("Fire1") > 0.01 && !AttackPressed && canAttack)
+        {
+            AttackPressed = true;
+            //Button pressed
+            StartCoroutine(chargeAttack());//Start a coroutine that will charge a attack over time
+        }
+        else if (Input.GetAxisRaw("Fire1") == 0 && AttackPressed == true)
+        {
+            AttackPressed = false;
+            //Button Released
+            releaseAttack();
+            StartCoroutine(AttackCooldown(1.0f));
+        }
     }
-    GetComponent<AnimationController>().ChangeAnim(SceneConstants.PunchReleaseAnim);
-    GetComponent<Attack>().AttackTarget(mult);//Send out the attack after the release
-    chargeAmount = 0;//reset the charge
-  }
+
+    private void FixedUpdate()
+    {
+        //Apply motion
+        //Set the velocity of the player's rigid body to the Velocity(input) * the change in time between frames
+        if (!Variables.isAttacking && GameManager.GAMEMANAGER.getCanMove())
+        {
+            PlayerRigidBody.velocity = Velocity * Time.deltaTime;
+        }
+        else
+        {
+            PlayerRigidBody.velocity = Vector2.zero;
+        }
+    }
+
+    IEnumerator chargeAttack()
+    {
+        Variables.isAttacking = true;
+        GetComponent<AnimationController>().ChangeAnim(SceneConstants.PunchChargeAnim);
+        while (chargeAmount < 100)
+        {
+            yield return new WaitForSeconds(chargeTime / 100);
+            chargeAmount += 1f;
+        }
+    }
+    private void releaseAttack()
+    {
+        StopAllCoroutines();//Stop charging the attack
+        float mult = (chargeAmount / 100) * chargeMultiplier; //Damage mutliplier for charge attack
+        if (mult < 1)
+        {//Damage will at least be 1
+            mult = 1;
+        }
+        GetComponent<AnimationController>().ChangeAnim(SceneConstants.PunchReleaseAnim);
+        GetComponent<Attack>().AttackTarget(mult);//Send out the attack after the release
+        chargeAmount = 0;//reset the charge
+    }
+
+
+    IEnumerator AttackCooldown(float duration)
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(duration); //wait for duration of attack
+        canAttack = true;
+    }
 }
