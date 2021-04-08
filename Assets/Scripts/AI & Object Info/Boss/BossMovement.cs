342f194edaa9cecf63377bc7edded5da0570844c
paynee7752 <77994244+paynee7752@users.noreply.github.com>
2021-04-08 17:33:39 -0500

    Boss Scripts Started
    Problem in foreach loop

--- Assets/Scripts/AI & Object Info/Boss/BossMovement.cs
+++ Assets/Scripts/AI & Object Info/Boss/BossMovement.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class BossMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform Player;
+    //int MaxDist = 10;
+    int MinDist = 10;
+    [SerializeField] private ActorVariables Variables;
+    [SerializeField] private Attack Attack;
+    bool isChasing = false;
+    bool isAttacking = false;
+    bool canAttack = false;
+    Vector3 movement = Vector3.zero;
+    [SerializeField] private Vector2 Direction;
+    int enemyTotal;
 
-    // Update is called once per frame
+    [SerializeField] private float cooldown;
     void Update()
     {
-        
+        enemyTotal = 0;
+        countEnemies();
+        float enemyDistance = Vector3.Distance(transform.position, Player.position);
+        if (enemyTotal == 0)
+        {
+            if (enemyDistance <= 30 && isChasing == false)
+            {
+                isChasing = true;
+            }
+            if (isChasing == true && GameManager.GAMEMANAGER.getCanMove())
+            {
+
+                if (enemyDistance >= MinDist && !GetComponent<ActorVariables>().isAttacking)
+                {
+                    Direction = (Player.position - transform.position).normalized;
+                    movement = Direction * Variables.moveSpeed * Time.deltaTime;
+                    movement.z = 0;
+                }
+                else
+                {
+                    Direction = Vector2.zero;
+                    movement = Vector3.zero;
+                    if (canAttack == false)
+                    {
+                        Debug.Log("attack player");
+                        GetComponent<AnimationController>().ChangeAnim(SceneConstants.Punch);
+                        StartCoroutine(attackCooldown());
+                        canAttack = true;
+                    }
+                }
+
+                GetComponent<ActorVariables>().Direction = Direction;
+                if (Direction.x != 0)
+                {
+                    GetComponent<ActorVariables>().AttackDirection = Direction;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += movement;
+    }
+
+    IEnumerator attackCooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canAttack = false;
+        Debug.Log("Finished Attacking player");
+    }
+
+    int countEnemies()
+    {
+        //I know that the GameObject.Find(...) should be proper, I'm just trying to make a good for each loop
+        foreach (Gameobject Enemy in GameObject.FindGameObjectsWithTag("Enemy").Length)
+        {
+            enemyTotal++;
+        }
+        return enemyTotal;
     }
 }
